185f840d7056f625bb7897dc8b59f5f7c5453782
Rimi999 <all_wom@naver.com>
2021-06-17 10:12:39 +0900

    section마다 함수들을 IIFE로 묶어줌

--- js/common.js
+++ js/common.js
@@ -1,18 +1,20 @@
-/*************** 글로벌 설정 *****************/
-var isHeaderAni = false
-initCommon()
+
+$(function() {
+	/*************** 글로벌 설정 *****************/
+
+	init()
 
 
-/*************** 사용자 함수 *****************/
-function initCommon() {
+	/*************** 사용자 함수 *****************/
+	function initCommon() {
 	if($.cookie('hideNotice') === 'Y') onCloseNotice()
 	else {
 		$('.notice-wrapper').find('.notice-content').hide()
 		$('.notice-wrapper').find('.bt-hide').hide()
 	}
-}
+	}
 
-function scrollNotice(scTop) {
+	function scrollNotice(scTop) {
 	var $notice = $('.notice-wrapper')
 	var $link = $('.link-wrapper')
 	var $header = $('.header-wrapper')
@@ -38,83 +40,84 @@ function scrollNotice(scTop) {
 		$header.css('top', 0)
 		$header.addClass('active')
 	}
-}
+	}
 
 
-/*************** 이벤트 등록 *****************/
-$(window).scroll(onScroll).trigger('scroll')
+	/*************** 이벤트 등록 *****************/
+	$(window).scroll(onScroll).trigger('scroll')
 
-$('.header-wrapper .navi').mouseenter(onNaviEnter)
-$('.header-wrapper .navi').mouseleave(onNaviLeave)
+	$('.header-wrapper .navi').mouseenter(onNaviEnter)
+	$('.header-wrapper .navi').mouseleave(onNaviLeave)
 
-$('.notice-wrapper .bt-show').click(onShowNotice)
-$('.notice-wrapper .bt-hide').click(onHideNotice)
-$('.notice-wrapper .bt-close').click(onCloseNotice)
-$('.notice-wrapper .bt-today').click(onHideTodayNotice)
+	$('.notice-wrapper .bt-show').click(onShowNotice)
+	$('.notice-wrapper .bt-hide').click(onHideNotice)
+	$('.notice-wrapper .bt-close').click(onCloseNotice)
+	$('.notice-wrapper .bt-today').click(onHideTodayNotice)
 
-$('.header-wrapper .link-lang').click(onToggleLang)
-$('.header-wrapper .link-lang').mouseenter(onShowLang)
-$('.header-wrapper .link-lang').mouseleave(onHideLang)
-$('.header-wrapper .link-lang .lang').click(onChgLang)
+	$('.header-wrapper .link-lang').click(onToggleLang)
+	$('.header-wrapper .link-lang').mouseenter(onShowLang)
+	$('.header-wrapper .link-lang').mouseleave(onHideLang)
+	$('.header-wrapper .link-lang .lang').click(onChgLang)
 
 
 
-/*************** 이벤트 콜백 *****************/
-function onNaviEnter() {
+	/*************** 이벤트 콜백 *****************/
+	function onNaviEnter() {
 	$('.header-wrapper .sub-wrapper').hide()
 	$(this).find('.sub-wrapper').show()
 	$('.header-wrapper .navi').removeClass('active')
 	$(this).addClass('active')
-}
+	}
 
-function onNaviLeave() {
+	function onNaviLeave() {
 	$('.header-wrapper .sub-wrapper').hide()
 	$('.header-wrapper .navi').removeClass('active')
-}
+	}
 
-function onScroll(e) {
+	function onScroll(e) {
 	var scTop = $(this).scrollTop()
 	scrollNotice(scTop)
-}
+	}
 
-function onShowNotice() {
+	function onShowNotice() {
 	$('.notice-wrapper').find('.bt-show').hide()
 	$('.notice-wrapper').find('.bt-hide').show()
 	$('.notice-wrapper').find('.notice-content').show()
-}
+	}
 
-function onHideNotice() {
+	function onHideNotice() {
 	$('.notice-wrapper').find('.bt-show').show()
 	$('.notice-wrapper').find('.bt-hide').hide()
 	$('.notice-wrapper').find('.notice-content').hide()
-}
+	}
 
-function onCloseNotice() {
+	function onCloseNotice() {
 	$('.notice-wrapper').hide()
-}
+	}
 
-function onHideTodayNotice() {
+	function onHideTodayNotice() {
 	$.cookie('hideNotice', 'Y', { expires: 1, path: '/' })
 	onCloseNotice()
-}
+	}
 
-function onToggleLang() {
+	function onToggleLang() {
 	$('.header-wrapper .link-lang .hover').toggle()
-}
+	}
 
-function onShowLang() {
+	function onShowLang() {
 	$('.header-wrapper .link-lang .hover').show()
-}
+	}
 
-function onHideLang() {
+	function onHideLang() {
 	$('.header-wrapper .link-lang .hover').hide()
-}
+	}
 
-function onChgLang() {
+	function onChgLang() {
 	var $span = $(this).parent().prev().find('span')
 	var myLang = $(this).text()
 	var spanLang = $span.text()
 	$span.text(myLang)
 	$(this).text(spanLang)
-}
+	}
 
+})
--- js/index.js
+++ js/index.js
@@ -1,28 +1,33 @@
-/*************** 글로벌 설정 *****************/
-var $mainWrapper = $('.main-wrapper')
-var $mainSlide = $('.main-wrapper .slide')
-var mainLen = $mainSlide.length
-var mainLastIdx = mainLen - 1
-var mainDepth = 2
-var mainIdx = 0
-var mainGap = 3000
-var mainSpeed = 500
-var mainInterval
-initMain()
+/*************** headerWrapper *****************/
+$(function() {
+
+	/*************** 글로벌 설정 *****************/
+	var $Wrapper = $('.main-wrapper')
+	var $Slide = $('.main-wrapper .slide')
+	var Len = $Slide.length
+	var LastIdx = Len - 1
+	var Depth = 2
+	var Idx = 0
+	var Gap = 3000
+	var Speed = 500
+	var Interval
+	init()
 
 
-/*************** 사용자 함수 *****************/
-function initMain() {
-	$mainSlide.eq(mainIdx).css('z-index', mainDepth++)
-	mainInterval = setInterval(onMainAni, mainGap)
-}
+	/*************** 사용자 함수 *****************/
+	function initMain() {
+		$Slide.eq(mainIdx).css('z-index', Depth++)
+		Interval = setInterval(oAni, Gap)
+	}
 
 
-/*************** 이벤트 등록 *****************/
+	/*************** 이벤트 등록 *****************/
 
 
-/*************** 이벤트 콜백 *****************/
-function onMainAni() {
-	mainIdx = (mainIdx == mainLastIdx) ? 0 : mainIdx + 1
-	$mainSlide.eq(mainIdx).css('z-index', mainDepth++)
-}
+	/*************** 이벤트 콜백 *****************/
+	function onAni() {
+		Idx = (Idx == LastIdx) ? 0 : Idx + 1
+		$Slide.eq(Idx).css('z-index', Depth++)
+	}
+
+})
