6f6fe3b2596750d4bc77119e0d47f5f0b5adc35f
Rimi999 <all_wom@naver.com>
2021-06-23 20:01:59 +0900

    util에 swiper 넣기

--- js/util.js
+++ js/util.js
@@ -13,22 +13,86 @@ function sortDesc(key) {
 
 
 
-/*************** Scroll Spy *****************/
-function scrollSpy(el, cls, _gap) {
-	$(window).scroll(onScrollSpy).trigger('scroll')
-	function onScrollSpy() {
-		var scrollTop = $(this).scrollTop()
-		var pageOffset = []
-		var page
-		var gap = _gap || 300
-		$(el).each(function(i){
-			pageOffset[i] = $(this).offset().top
-		})
-	
-		for(var i=1; i<pageOffset.length; i++) {
-			if(scrollTop < pageOffset[i] - gap) break
-		}
-		page = i - 1
-		$(el).eq(page).addClass(cls)
+/*************** getSwiper *****************/
+function getSwiper(el, opt) {
+	/*
+	- cls : '.promo-wrapper
+	- opt 
+	{
+		pager: true,
+		navi: true,
+		auto: true,
+		autoEl: '.slide-stage'
+		delay: 3000,
+		loop: true,
+		space: 40,
+		break: 1
 	}
+	*/
+	var opt = opt || {}
+	var autoEl = el + ' ' + (opt.autoEl || '.slide-stage')
+	var pagination = (opt.pager === false) ? false : {
+		el: el + ' .pager-wrapper',
+		clickable: true
+	}
+
+	var navigation = (opt.navi === false) ? false : {
+		nextEl: el + ' .bt-slide.right',
+		prevEl: el + ' .bt-slide.left',
+	}
+
+	var autoplay = (opt.auto === false) ? false : {
+		// delay: opt.delay ? opt.delay : 3000
+		delay: opt.delay || 3000
+	}
+
+	var breakpoints = {};
+	if(opt.break == 2) {
+		breakpoints = {
+			768: { slidesPerView: 2}
+		}
+	}
+	else if(opt.break == 3) {
+		breakpoints = {
+			768: { slidesPerView: 2},
+			1200: { slidesPerView: 3}
+		}
+	}
+	else if(opt.break == 4) {
+		breakpoints = {
+			576: { slidesPerView: 2},
+			992: { slidesPerView: 3},
+			1200: { slidesPerView: 4}
+		}
+	}
+	else if(opt.break == 5) {
+		breakpoints = {
+			576: { slidesPerView: 2},
+			768: { slidesPerView: 3},
+			992: { slidesPerView: 4},
+			1200: { slidesPerView: 5}
+		}
+	}
+
+	var swiper = new Swiper(el + ' .swiper-container', {
+		pagination: pagination,
+		navigation: navigation,
+		autoplay: autoplay,
+		loop: opt.loop === false ? false : true,
+		slidesPerView: 1,
+		spaceBetween: opt.space || 40,
+		breakpoints: breakpoints
+	})
+
+	$(autoEl).hover(function(){
+		swiper.autoplay.stop()
+	}, function(){
+		swiper.autoplay.start()
+	})
+
+	return swiper
+}
+
+function swiperHover(swiper, el) {
+	
 }
